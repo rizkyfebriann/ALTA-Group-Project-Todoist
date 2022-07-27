f96eacbf1f3856f96347cf7ff659bc93e4bed690
Wisnu Munawar <54229493+wisnuwm@users.noreply.github.com>
2022-07-27 21:18:48 +0700

    fix create project

--- src/test/java/Todoist/TodoistTesting/TodoistTesting.java
+++ src/test/java/Todoist/TodoistTesting/TodoistTesting.java
@@ -8,17 +8,19 @@ import net.thucydides.core.annotations.Step;
 
 public class TodoistTesting {
 
-    public static final String URL = "https://api.todoist.com/rest/v1/";
+    public static final String URL = "https://api.todoist.com";
     public static final String DIR = System.getProperty("user.dir");
 
     public static final String FILE_JSON = DIR+"/src/test/resources/fileJSONtodoist";
 
-    public static String CREATE_A_PROJECT = URL+"/projects";
+    public static String CREATE_A_PROJECT = URL+"/rest/v1/projects";
 
     @Step("Post create project headers")
-    public void postCreateProjectheader(){
+    public void postCreateProjectheader(File json){
         SerenityRest.given()
-                .headers("Authorization","Bearer cf3181ac95fcf0abcdcd08aa1ddafeba7824cabd");
+                    .contentType(ContentType.JSON)
+                    .body(json)
+                    .headers("Authorization","Bearer ba39cc2788122df9d25f70d46b2bbfb778afbbc0");
     }
 
     @Step("Post create project")
