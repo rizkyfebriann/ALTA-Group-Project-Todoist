5c6dddb19670d27a0b2a50e78466ccb43f2790f1
erna94 <erna9470@gmail.com>
2022-07-28 17:56:44 +0700

    Perubahan erna

--- src/test/java/Todoist/API/TodoistTesting.java
+++ src/test/java/Todoist/API/TodoistTesting.java
@@ -1,4 +1,4 @@
-package Todoist.TodoistTesting;
+package Todoist.API;
 
 import java.io.File;
 
@@ -15,7 +15,12 @@ public class TodoistTesting {
 
     public static String CREATE_A_PROJECT = URL+"/rest/v1/projects";
 
-    public static String GET_A_PROJECT = URL+"/rest/v1/projects";
+    public static String GET_ALL_PROJECT = URL+"/rest/v1/projects";
+    public static String GET_ALL_PROJECT_INVALID_PATH = URL+"/rest/v1/projects/xxx";
+    public static String GET_A_PROJECT = URL+"/rest/v1/projects/{id}";
+    public static String GET_A_PROJECT_INVALID_PATH = URL+"/rest/v1/projects/{id}/xx";
+    public static String GET_ALL_COLLABORATORS = URL+"/rest/v1/projects/{id}/collaborators";
+
 
     public String PROJECTS = "https://api.todoist.com/rest/v1/projects";
 
@@ -36,9 +41,21 @@ public class TodoistTesting {
 //    }
 
     @Step("Get all project")
-    public void getAllProject(){
+    public void getProjectHeader(){
         SerenityRest.given()
                 .headers("Authorization","Bearer cf3181ac95fcf0abcdcd08aa1ddafeba7824cabd");
+
+    }
+    @Step("get project headers invalid token")
+    public void getProjectHeaderInvalidToken(){
+        SerenityRest.given()
+                .headers("Authorization","Bearer 38e03e89042843309ca5a77775d69818a79f3b24xxx");
     }
 
+    @Step("Get a project with id")
+    public void getAProjectHeader(String id){
+        SerenityRest.given()
+                .headers("Authorization","Bearer 38e03e89042843309ca5a77775d69818a79f3b24")
+                .pathParam("id", id);
+    }
 }
--- src/test/java/Todoist/StepDefs/TodoistStepDefs.java
+++ src/test/java/Todoist/StepDefs/TodoistStepDefs.java
@@ -1,15 +1,15 @@
 package Todoist.StepDefs;
 
-import Todoist.TodoistTesting.TodoistTesting;
+import Todoist.API.TodoistTesting;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import java.io.File;
+
 import io.restassured.module.jsv.JsonSchemaValidator;
 import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.Steps;
-import static org.hamcrest.Matchers.equalTo;
 
 public class TodoistStepDefs {
 
@@ -36,20 +36,70 @@ public class TodoistStepDefs {
 //        File jsonFiles = new File(TodoistTesting.FILE_JSON+"/CreateProjectWithValidAuthorization.json");
 //        todoistTesting.postCreateProjectheader(jsonFiles);
 //    }
-
-    @When("Send request get all project")
-    public void sendRequestGetAllProject() {
-        SerenityRest.when().get(TodoistTesting.GET_A_PROJECT);
-    }
-
     @Given("post create project with valid json file")
     public void postCreateProjectWithValidJsonFile() {
         File jsonFiles = new File(TodoistTesting.FILE_JSON+"/CreateProjectWithValidAuthorization.json");
         todoistTesting.postCreateProjectheader(jsonFiles);
     }
 
-    @Given("request get all project")
-    public void requestGetAllProject() {
-        todoistTesting.getAllProject();
+//Get All Project
+    @Given("Set authorization with valid bearer token")
+    public void SetAuthorizationWithValidBeareToken() {
+        todoistTesting.getProjectHeader();
+    }
+    @When("Send request get all project")
+    public void sendRequestGetAllProject() {
+        SerenityRest.when().get(TodoistTesting.GET_ALL_PROJECT);
+    }
+
+    @And("Get all projects assert json validation")
+    public void getAllProjectsAssertJsonValidation() {
+        File jsonFile = new File(TodoistTesting.FILE_JSON+"/GetAllProjectJsonValidation.json");
+        SerenityRest.then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(jsonFile));
+    }
+//Get all project with invalid token
+    @Given("Set authorization with invalid bearer token")
+    public void settingAuthorizationWithInvalidBearerToken() {
+        todoistTesting.getProjectHeaderInvalidToken();
+    }
+
+    @Then("API response status code should be {int} Unauthorized")
+    public void apiResponseStatusCodeShouldBeUnauthorized(int Unauthorized) {
+        SerenityRest.then().statusCode(401);
+    }
+    //Get All Project With Invalid Path
+    @When("Send request get all project with invalid path")
+    public void sendRequestGetAllProjectWithInvalidPath() {
+        SerenityRest.when().get(TodoistTesting.GET_ALL_PROJECT_INVALID_PATH);
+    }
+    @Then("API response status code should be {int} Not Found")
+    public void apiResponseStatusCodeShouldBeNotFound(int NotFound) {
+        SerenityRest.then().statusCode(404);
+    }
+
+    //Get A Project with valid id
+    @Given("request get a project with valid id {string}")
+    public void requestGetAProjectWithValidId(String id) {
+        todoistTesting.getAProjectHeader(id);
+    }
+    @When("Send request get a project")
+    public void sendRequestGetAProject() {
+        SerenityRest.when().get(TodoistTesting.GET_A_PROJECT);
+    }
+    @And("Get a projects assert json validation")
+    public void getAProjectsAssertJsonValidation() {
+        File jsonFile = new File(TodoistTesting.FILE_JSON+"/GetAProjectJsonValidation.json");
+        SerenityRest.then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(jsonFile));
+    }
+
+    //Get A Project with invalid id
+    @Given("request get a project with invalid id {string}")
+    public void requestGetAProjectWithInvalidId(String id) {
+        todoistTesting.getAProjectHeader(id);
+    }
+    //Get All Collaborators
+    @When("Send request get all collaborators")
+    public void sendRequestGetAllCollaborators() {
+        SerenityRest.when().get(TodoistTesting.GET_ALL_COLLABORATORS);
     }
 }
